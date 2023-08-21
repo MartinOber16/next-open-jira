16b390d5831f8debd29359513336c0c93810e370
Martin Obermeier <martin.obermeier@gmail.com>
2023-08-21 17:10:05 -0300

    Fin seccion 7: OpenJira / MaterialUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import { darkTheme, lightTheme } from '@/themes';
+import { UIProvider } from '@/context/ui';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+	return (
+		<UIProvider>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</UIProvider>
+	);
 }
+
+export default App;
